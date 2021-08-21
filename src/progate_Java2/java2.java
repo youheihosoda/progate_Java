be5587d0f090c0e2c3eab0c82b9abdc94cdda45f
YouheiHosoda <youhe0122@gmail.com>
2021-08-21 12:19:06 +0900

    8/20

---
+++ src/progate_Java2/java2.java
@@ -0,0 +1,13 @@
+package progate_Java2;
+
+public class java2 {
+	 public static void main(String[] args) {
+		    // 8と5を比較し、falseとなるようにしてください
+		    System.out.println(8 < 5);
+		    
+		    // 3と2を比較し、trueとなるようにしてください
+		    System.out.println(3 >= 2);
+		    
+		  }
+
+}
