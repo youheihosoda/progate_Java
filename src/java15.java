6bbd23136ba7f12fd3ad9f908f69386a5c8b2913
YouheiHosoda <youhe0122@gmail.com>
2021-09-01 23:06:11 +0900

    9/1

---
+++ src/java15.java
@@ -0,0 +1,33 @@
+
+public class java15 {
+	 public static void main(String[] args) {
+		    Scanner scanner = new Scanner(System.in);
+		    System.out.print("名前：");
+		    String firstName = scanner.next();
+		    
+		    System.out.print("名字：");
+		    String lastName = scanner.next();
+		    
+		    System.out.print("年齢：");
+		    int age = scanner.nextInt();
+		    
+		    System.out.print("身長(m)：");
+		    double height = scanner.nextDouble();
+		    
+		    System.out.print("体重(kg)：");
+		    double weight = scanner.nextDouble();
+		    
+		    System.out.println("名前は" + firstName + "" + lastName + "です");
+		    System.out.println("年齢は" + age + "歳です");
+		    
+		    if(age >= 20){
+		      System.out.println("成年者です");
+		    }else{
+		      System.out.println("未成年者です");
+		    }
+		    
+		    System.out.println("身長は" + height + "mです");
+		    System.out.println("体重は" + weight + "kgです");
+		  }
+
+}
